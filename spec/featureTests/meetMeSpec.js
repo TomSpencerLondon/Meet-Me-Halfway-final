4d061298ab44c4324f1cc34b1e6361f518e426af
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-12-21 14:29:45 +0000

    Add test for Map Div

--- spec/featureTests/meetMeSpec.js
+++ spec/featureTests/meetMeSpec.js
@@ -3,7 +3,7 @@ const request = require('request');
 const app = require('../../app');
 
 Browser.localhost('example.com', 3001);
-const timeout = jasmine.DEFAULT_TIMEOUT_INTERVAL; 
+const timeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
 const longTimeout = timeout * 10;
 
 describe('website', function(){
@@ -27,5 +27,11 @@ describe('website', function(){
 
   }, timeout);
 
+  it('should contain a map div', function(done){
+    browser.visit("/", function(err){
+      expect(browser.html("body")).toContain('<div id="map"></div>')
+      done();
+    });
+  }, timeout);
 
-})
+});
